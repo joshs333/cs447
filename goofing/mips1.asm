8e41b6e092fe47f63e653b2fd9cd6f230cc5a39c
joshua.spisak <joshs333@live.com>
2019-03-19 13:45:01 -0400

    Removed temp/zip files/scrap files Isolated projects/labs in different folders

--- goofing/mips1.asm
+++
@@ -1,5 +0,0 @@
-.text
-	li $t1, 3
-        li $t3, 8
-        add 
-        move $t2, ($t0 + 3)
--- projects/java-assembler-project/test.sh
+++ projects/java-assembler-project/test.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 ##############################################################################
 # Joshua Spisak
 # joshs333@live.com / jjs231@pitt.edu
@@ -8,7 +7,8 @@
 #
 # test.sh -r : compiles project and runs tests
 ##############################################################################
-SRC_DIR=./
+#!/bin/bash
+SRC_DIR=./src/
 INCLUDE=.:include/hamcrest-all-1.3.jar:include/junit-4.13-beta-2.jar
 RUN_INCLUDE=.:../include/hamcrest-all-1.3.jar:../include/junit-4.13-beta-2.jar
 BUILD_DIR=class
@@ -17,30 +17,31 @@ JAR_PACKAGE=org.junit.runner.JUnitCore
 JAR_MAIN=AssemblerTest
 
 # See if build already exists
-if [ -d "$BUILD_DIR" ] || [ -f "$BUILD_JAR" ]; then
-    # Make old_build folder if !exist
-    if [ ! -d "old_$BUILD_DIR" ]; then
-        mkdir old_$BUILD_DIR
-    else
-	    # either build or Solver.jar exists
-        # we only want to remove everything
-        # if we have a replacement Solver.jar
-        if [ -f "$BUILD_JAR" ]; then
-            rm -rf old_$BUILD_DIR/
-            mkdir old_$BUILD_DIR
-        else
-            rm -rf old_$BUILD_DIR/$BUILD_DIR/
-        fi
-    fi
-
-    # Now that we have a backup folder let's store stuff
-    if [ -d "$BUILD_DIR" ]; then
-        mv $BUILD_DIR/ old_$BUILD_DIR/$BUILD_DIR/
-    fi
-
-    if [ -f "$BUILD_JAR" ]; then
-        mv $BUILD_JAR old_$BUILD_DIR/
-    fi
+if [ -d "$BUILD_DIR" ]; then #|| [ -f "$BUILD_JAR" ]; then
+    rm -rf $BUILD_DIR/
+#    # Make old_build folder if !exist
+#    if [ ! -d "old_$BUILD_DIR" ]; then
+#        mkdir old_$BUILD_DIR
+#    else
+#	    # either build or Solver.jar exists
+#        # we only want to remove everything
+#        # if we have a replacement Solver.jar
+#        if [ -f "$BUILD_JAR" ]; then
+#            rm -rf old_$BUILD_DIR/
+#            mkdir old_$BUILD_DIR
+#        else
+#            rm -rf old_$BUILD_DIR/$BUILD_DIR/
+#        fi
+#    fi
+#
+#    # Now that we have a backup folder let's store stuff
+#    if [ -d "$BUILD_DIR" ]; then
+#        mv $BUILD_DIR/ old_$BUILD_DIR/$BUILD_DIR/
+#    fi
+#
+#    if [ -f "$BUILD_JAR" ]; then
+#        mv $BUILD_JAR old_$BUILD_DIR/
+#    fi
 fi
 
 # make new build directory
